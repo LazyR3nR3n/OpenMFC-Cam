048851dac9a70d2c75f03efee554a730895fbca2
LazyR3nR3n <rencecastillo1216@gmail.com>
2026-05-11 10:53:48 +0800

    hide console window on launch

--- src/main.py
+++ src/main.py
@@ -4,6 +4,10 @@ import ctypes
 from devices import enumerate_cameras, get_camera_labels
 from enhance import load_model
 import sys
+import ctypes
+
+ctypes.windll.kernel32.FreeConsole()
+
 if sys.platform == "win32":
     import ctypes
     ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
